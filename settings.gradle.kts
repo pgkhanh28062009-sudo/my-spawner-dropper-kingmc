fad493207bec1967002dc6b432ec77b973eb24d8
pgkhanh28062009-sudo <pgkhanh28062009@gmail.com>
2026-07-26 13:33:02 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,9 @@
 pluginManagement {
     repositories {
-        maven {
-            name = "Fabric"
-            url = uri("https://maven.fabricmc.net/")
-        }
-        mavenCentral()
+        maven("https://maven.meteorclient.com/")
         gradlePluginPortal()
+        mavenCentral()
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
-rootProject.name = "addon-template"
+rootProject.name = "my-spawner-dropper-kingmc"
